678973f8877d570d9f6aa2af042076de8a3c3e0e
alinsyarif <alinsyarif04@gmail.com>
2024-11-29 12:58:04 +0700

    update env file

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
 // const serviceAccount = require("../ServiceAccount_Firebase.json");
+require('dotenv').config()
 
 const serviceAccount = JSON.parse(
   Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY, 'base64').toString('utf-8')
