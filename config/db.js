bb7c5b348c2abfa800575d318fbdea5dab293e81
alinsyarif <alinsyarif04@gmail.com>
2024-11-29 12:55:40 +0700

    Perubahan setting deploy

--- config/db.js
+++ config/db.js
@@ -1,10 +1,17 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../ServiceAccount_Firebase.json");
+// const serviceAccount = require("../ServiceAccount_Firebase.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.URLDATABASE,
-});
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY, 'base64').toString('utf-8')
+)
+
+if (!admin.apps.length){
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.URLDATABASE,
+  });
+}
+
 
 const db = admin.firestore();
 module.exports = { admin, db };
