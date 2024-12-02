b2872679343a1bc84a87545436bf65633696aeef
HananRafiHadi <121382439+HananRafiHadi@users.noreply.github.com>
2024-12-02 12:36:20 +0700

    del

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const plantRoutes = require('./routes/plantRoutes');
-const solutionRoutes = require('./routes/solutionRoutes');
